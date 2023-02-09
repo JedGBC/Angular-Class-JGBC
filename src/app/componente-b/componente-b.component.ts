e4c5e16c6ea9b37b2631f284a7b59aa1b48572b9
JedGBC <jedguard@gmail.com>
2023-02-08 21:10:10 -0300

    Second commit

--- src/app/componente-b/componente-b.component.ts
+++ src/app/componente-b/componente-b.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JuegosService } from '../servicios/juegos.service';
 
 @Component({
   selector: 'app-componente-b',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./componente-b.component.css']
 })
 export class ComponenteBComponent {
+  constructor(private servicioJuegos:JuegosService){}
 
+  ngOnInit(){
+    console.log("Inicio del componente B - gatillador de actividades");
+    this.obtenerJuegos();
+  }
+
+  obtenerJuegos(){
+    console.log("Lista de Objetos: ",this.servicioJuegos.getData());
+
+    for (let i = 0; i < this.servicioJuegos.getData().length; i++) {
+      const element = this.servicioJuegos.getData()[i];
+      console.log("Juego #",i,": ",element);
+    }
+  }
 }
